6de90318351a2813165eef0638b669ec62dec802
Pijush Konar <59543014+pijush-konar-au7@users.noreply.github.com>
2020-09-26 13:07:47 +0530

    Started Building Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class  extends Component {
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
+const styles = theme => ({
+    card: {
+      minWidth: 300,
+      margin: 100
+    }
+});
+
+class Dashboard extends Component {
     render() {
+        const { user } = this.props.auth;
+
         return (
-            <div>
-                <h1>This is Dashboard</h1>
-            </div>
+            <React.Fragment>
+                <Typography variant="h4" component="h1" align="center">
+                    Welcome, {user.firstname}!
+                </Typography>
+            </React.Fragment>
         )
     }
 }
 
+const mapStateToProps = state => ({
+    auth: state.auth
+});
 
-
-
-
-
+export default connect(mapStateToProps)(withStyles(styles)(Dashboard));
